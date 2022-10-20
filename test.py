962764920b7d3648ca396e1acea41de2dd5a5526
kravshan <pasidub23@gmail.com>
2022-10-20 20:15:55 +0530

    init

--- test.py
+++ test.py
@@ -2,18 +2,37 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-number_1 = 12345
-response = requests.put(BASE + "account/" + str(number_1), {"account_number": number_1, "balance": 900})
+#Creating new accounts
+response = requests.put(BASE + "account/10", {"account_number": 10, "balance": 500})
 print(response.json())
 
-# response = requests.put(BASE + "account/54321", {"balance": 300})
-# print(response.json())
-
-response = requests.get(BASE + "account/12345")
+response = requests.put(BASE + "account/11", {"account_number": 10, "balance": 700})
 print(response.json())
 
-# response = requests.delete(BASE + "account/2")
-# print(response.json())
+response = requests.put(BASE + "account/12", {"account_number": 10, "balance": 700})
+print(response.json())
 
-# response = requests.get(BASE + "account/2")
-# print(response.json())
+#Reading created accounts
+response = requests.get(BASE + "account/10")
+print(response.json())
+
+response = requests.get(BASE + "account/11")
+print(response.json())
+
+response = requests.get(BASE + "account/12")
+print(response.json())
+
+#Updating some accounts
+response = requests.put(BASE + "account/10", {"balance": 890})
+print(response.json())
+
+response = requests.put(BASE + "account/12", {"balance": 2400})
+print(response.json())
+
+#Deleting an account
+response = requests.delete(BASE + "account/12")
+print(response.json())
+
+#Trying to read the deleted account
+response = requests.get(BASE + "account/12")
+print(response.json())
